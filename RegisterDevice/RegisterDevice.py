71e2d994f48dc6f59b7aefa5f87f865b12286035
rzadp <rzadp@student.mini.pw.edu.pl>
2015-11-13 17:04:36 +0100

    Working registration

--- RegisterDevice/RegisterDevice.py
+++ RegisterDevice/RegisterDevice.py
@@ -1,22 +1,22 @@
 #!/bin/python2
-from WebApiClient.py import *
+from WebApiClient import *
 
 print "Witaj w skrypcie rejestrujacym urzadzenie RaspberryPi"
 username = raw_input("Podaj nazwe uzytkownika: ")
 password = raw_input("Podaj haslo: ")
 name = raw_input("Podaj nazwe (np. Raspberak w kuchni)")
 print "Podaj kolor, ktory bedzie wyswietlany w aplikacjach. "
-color = raw_raw_input("Podaj jako HEX (np. #FF0000): ")
+color = raw_input("Podaj jako HEX (np. #FF0000): ")
 
 print 'Zaczynam probe rejestracji.'
-(device_token, result) = WRH.register_device(username, password, name, color)
+(status_code, result_content) = register_device(username, password, name, color)
 print 'Skonczylem probe.'
 
-if (result == Response.STATUS_OK):
-    print 'Udalo sie!' + str(device_token)
+if (int(status_code) == Response.STATUS_OK):
+    print 'Udalo sie!' + str(result_content)
 else:
-    print 'Nie udalo sie. Status code: ' + str(result)
-    print '' + str(device_token)
+    print 'Nie udalo sie. Status code: ' + str(status_code)
+    print 'Content: ' + str(result_content)
 
 
 
--- WebApiLibrary/WebApiClient.py
+++ WebApiLibrary/WebApiClient.py
@@ -9,41 +9,40 @@ class Response(Enum):
     STATUS_BAD_REQUEST = 400
     INTERNAL_SERVER_ERROR = 500
     
-class WRH:
-	headers = {'content-type': 'application/json'}
-	base_address = 'https://wildraspberrywebapi.azurewebsites.net/'
-	register_device_url = base_address + 'api/wrh/registerdevice'
-	add_module_url = base_address + 'api/wrh/addmodule'
-	edit_module_url = base_address + ''
-	remove_module_url = base_address + ''
-	get_scenarios_url = base_address + ''
-	add_measurement_url = base_address + ''
-		
-	def do_post_request(url, content):
-		response = requests.post(url, data = json.dumps(content), headers = headers)
-		return (response.status_code, response.text)
+headers = {'content-type': 'application/json'}
+base_address = 'https://wildraspberrywebapi.azurewebsites.net/'
+register_device_url = base_address + 'api/wrh/registerdevice'
+add_module_url = base_address + 'api/wrh/addmodule'
+edit_module_url = base_address + ''
+remove_module_url = base_address + ''
+get_scenarios_url = base_address + ''
+add_measurement_url = base_address + ''
+	
+def do_post_request(url, content):
+	response = requests.post(url, data = json.dumps(content), headers = headers)
+	return (response.status_code, response.text)
 
-	def register_device(username, password, device_name, device_color):
-		content = {'Username': username, 'Password': password, 'Name': device_name, 'Color': device_color}
-		return do_post_request(register_device_url, content)
+def register_device(username, password, device_name, device_color):
+	content = {'Username': username, 'Password': password, 'Name': device_name, 'Color': device_color}
+	return do_post_request(register_device_url, content)
 
-	def add_module(device_id, device_token, module_name, module_type):
-		content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
-		return do_post_request(add_module_url, content)
+def add_module(device_id, device_token, module_name, module_type):
+	content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
+	return do_post_request(add_module_url, content)
 
-	def edit_module(device_id, device_token, module_id,  module_name):
-		content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
-		return do_post_request(add_module_url, content)
-		
-	def remove_module(device_id, device_token, module_id):
-		content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
-		return do_post_request(add_module_url, content)
-		
-	def get_scenarios(device_id, device_token):
-		content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
-		return do_post_request(add_module_url, content)
-		
-	def add_measurement(device_id, device_token, module_id, timestamp, value, streamingaddress):
-		content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
-		return do_post_request(add_module_url, content)
+def edit_module(device_id, device_token, module_id,  module_name):
+	content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
+	return do_post_request(add_module_url, content)
+	
+def remove_module(device_id, device_token, module_id):
+	content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
+	return do_post_request(add_module_url, content)
+	
+def get_scenarios(device_id, device_token):
+	content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
+	return do_post_request(add_module_url, content)
+	
+def add_measurement(device_id, device_token, module_id, timestamp, value, streamingaddress):
+	content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
+	return do_post_request(add_module_url, content)
 
