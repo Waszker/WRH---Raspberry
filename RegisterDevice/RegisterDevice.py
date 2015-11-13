bc5eb320d9e4ce27e24d83f35435abb4a74c2a6a
waszker <waszka23@gmail.com>
2015-11-13 16:34:32 +0100

    Changed RegisterDevice script

--- RegisterDevice/RegisterDevice.py
+++ RegisterDevice/RegisterDevice.py
@@ -1,34 +1,22 @@
-import requests
-import json
+#!/bin/python2
+from WebApiClient.py import *
 
-baseaddress = 'https://wildraspberrywebapi.azurewebsites.net/'
+print "Witaj w skrypcie rejestrujacym urzadzenie RaspberryPi"
+username = raw_input("Podaj nazwe uzytkownika: ")
+password = raw_input("Podaj haslo: ")
+name = raw_input("Podaj nazwe (np. Raspberak w kuchni)")
+print "Podaj kolor, ktory bedzie wyswietlany w aplikacjach. "
+color = raw_raw_input("Podaj jako HEX (np. #FF0000): ")
 
-print("Skrypt rejestrujacy Urzadzenie Raspberry Pi. ")
+print 'Zaczynam probe rejestracji.'
+(device_token, result) = WRH.register_device(username, password, name, color)
+print 'Skonczylem probe.'
 
-username = input("Podaj nazwe uzytkownika: ")
-password = input("Podaj haslo: ")
-name = input("Podaj nazwe (np. Raspberak w kuchni)")
-print("Podaj kolor, ktory bedzie wyswietlany w aplikacjach. ")
-color = input("Podaj jako HEX (np. #FF0000): ")
-#username = 'test@wp.pl'
-#password = '123'
-#name = 'raspberry pi'
-#color = 'FF0000'
-
-
-url = baseaddress + 'api/wrh/registerdevice'
-content = {'Username': username, 'Password': password, 'Name': name, 'Color': color}
-headers = {'content-type': 'application/json'}
-
-print('Zaczynam probe rejestracji.')
-response = requests.post(url, data = json.dumps(content), headers = headers)
-print('Skonczylem probe.')
-
-if (response.status_code == 200):
-    print('Udalo sie!')
+if (result == Response.STATUS_OK):
+    print 'Udalo sie!' + str(device_token)
 else:
-    print('Nie udalo sie. Status code: ' + str(response.status_code))
-    print(response.text)
+    print 'Nie udalo sie. Status code: ' + str(result)
+    print '' + str(device_token)
 
 
 
