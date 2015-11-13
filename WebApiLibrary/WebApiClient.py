a81b2ee741b61bb62c8847fc2134782d5ba959a2
254209 rzad przemyslaw <rzadp@p21807.mini.pw.edu.pl>
2015-11-13 16:51:17 +0100

    api

--- WebApiLibrary/WebApiClient.py
+++ WebApiLibrary/WebApiClient.py
@@ -14,10 +14,10 @@ class WRH:
 	base_address = 'https://wildraspberrywebapi.azurewebsites.net/'
 	register_device_url = base_address + 'api/wrh/registerdevice'
 	add_module_url = base_address + 'api/wrh/addmodule'
-	edit_module_url = base_address + ''
-	remove_module_url = base_address + ''
-	get_scenarios_url = base_address + ''
-	add_measurement_url = base_address + ''
+	edit_module_url = base_address + 'api/wrh/editmodule'
+	remove_module_url = base_address + 'api/wrh/removemodule'
+	get_scenarios_url = base_address + 'api/wrh/getscenariosdevice'
+	add_measurement_url = base_address + 'api/wrh/addmeasurement'
 		
 	def do_post_request(url, content):
 		response = requests.post(url, data = json.dumps(content), headers = headers)
@@ -32,15 +32,15 @@ class WRH:
 		return do_post_request(add_module_url, content)
 
 	def edit_module(device_id, device_token, module_id,  module_name):
-		content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
+		content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Id': module_id}
 		return do_post_request(add_module_url, content)
 		
 	def remove_module(device_id, device_token, module_id):
-		content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
+		content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Id': module_id}
 		return do_post_request(add_module_url, content)
 		
 	def get_scenarios(device_id, device_token):
-		content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
+		content = {'Id': device_id, 'Token': device_token}
 		return do_post_request(add_module_url, content)
 		
 	def add_measurement(device_id, device_token, module_id, timestamp, value, streamingaddress):
