c9284e33be6fc2057b68a201a4e8c029cc5916ab
Piotr Waszkiewicz <waszka23@gmail.com>
2015-11-15 19:07:59 +0100

    Added configuration file parser.

--- WRH_Engine/Configuration/configuration.py
+++ WRH_Engine/Configuration/configuration.py
@@ -8,7 +8,7 @@ def check_configuration_file_sanity(file_handler) :
     next_lines_pattern = re.compile("(([1-9][0-9]{0,9};){3})(.+?);(.*)$")
 
     for i, line in enumerate(file_handler) :
-        if(i == 0) :
+        if i == 0 :
             does_match = first_line_pattern.match(line)
             if not does_match :
                 print 'First line of config file is corrupted!'
@@ -21,3 +21,26 @@ def check_configuration_file_sanity(file_handler) :
 
 
     return does_match
+
+def _get_first_line_data(line) :
+    matches = re.search("([1-9][0-9]{0,9});(.+)$", line)
+    return (matches.group(1), matches.group(2))
+
+def _get_module_entry_date(line) :
+    m = re.search("([1-9][0-9]{0,9});([1-9][0-9]{0,9});([1-9][0-9]{0,9});(.+?);(.*)$", line)
+    return (m.group(1), m.group(2), m.group(3), m.group(4), m.group(5))
+
+# This function does not check if configuration file is sane
+# Please do check it before invoking!
+# Returns tuple containing tuple of device id and device token
+# and list of modules
+def parse_configuration_file(file_handler) :
+    (device_id, device_token) = _get_first_line_data(file_handler.readline())
+
+    modules_list = []
+    for i, line in enumerate(file_handler) :
+        if i == 0 : continue
+        modules_list.append(_get_module_entry_date(line))
+
+    return ((device_id, device_token), modules_list)
+
