65e77b84c38d59daed593eed16d6a38adffd758f
Piotr Waszkiewicz <waszka23@gmail.com>
2015-11-17 00:07:35 +0100

    Updated config script

--- script.py
+++ script.py
@@ -6,11 +6,33 @@ from WRH_Engine.Configuration import configuration as config
 
 CONFIGURATION_FILE = '.wrh.config'
 
+def _edit_module(modules) :
+    print 'Editing module'
+
+def _add_new_module() :
+    print 'Adding new module'
+
 def _run_maintenance_work():
     with open(CONFIGURATION_FILE, 'r') as f:
         (system_info, modules) = config.parse_configuration_file(f)
+        print '\n=== LIST OF REGISTERED MODULES==='
         for module in modules :
             config.print_module_information(module)
+        print '\n[1] Edit module\n[2] Add new module\n[3] Exit'
+
+        while True :
+            user_choice = raw_input('> ')
+            try :
+                value = int(user_choice)
+            except ValueError:
+                continue
+            if value == 1 :
+                _edit_module(modules)
+            elif value == 2 :
+                _add_new_module()
+            elif value == 3 :
+                break
+            continue
     return
 
 def _is_configuration_file_sane():
