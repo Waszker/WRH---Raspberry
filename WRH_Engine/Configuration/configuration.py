4e0c324e8349148fa041fd18e7d4a320fe342d24
Piotr Waszkiewicz <waszka23@gmail.com>
2015-11-16 23:40:25 +0100

    Created module listing

--- WRH_Engine/Configuration/configuration.py
+++ WRH_Engine/Configuration/configuration.py
@@ -6,6 +6,7 @@ def check_configuration_file_sanity(file_handler) :
     # Other lines are for module entries
     first_line_pattern = re.compile("([1-9][0-9]{0,9});(.+)$")
     next_lines_pattern = re.compile("(([1-9][0-9]{0,9};){3})(.+?);(.*)$")
+    does_match = False
 
     for i, line in enumerate(file_handler) :
         if i == 0 :
@@ -26,7 +27,7 @@ def _get_first_line_data(line) :
     matches = re.search("([1-9][0-9]{0,9});(.+)$", line)
     return (matches.group(1), matches.group(2))
 
-def _get_module_entry_date(line) :
+def _get_module_entry_data(line) :
     m = re.search("([1-9][0-9]{0,9});([1-9][0-9]{0,9});([1-9][0-9]{0,9});(.+?);(.*)$", line)
     return (m.group(1), m.group(2), m.group(3), m.group(4), m.group(5))
 
@@ -39,8 +40,29 @@ def parse_configuration_file(file_handler) :
 
     modules_list = []
     for i, line in enumerate(file_handler) :
-        if i == 0 : continue
-        modules_list.append(_get_module_entry_date(line))
+        modules_list.append(_get_module_entry_data(line))
 
     return ((device_id, device_token), modules_list)
 
+def get_module_type_string(module) :
+    type = int(module[0])
+    type_string = "UNKNOWN"
+    if type == 1 :
+        type_string = "DHT"
+    elif type == 2 :
+        type_string = "Camera"
+    elif type == 3 :
+        type_string = "Motion"
+    elif type == 4 :
+        type_string = "Wi-Fi Socket"
+
+    return type_string
+
+def print_module_information(module) :
+    type = int(module[0])
+    if type == 1 or type == 3 :
+        print (module[3] + ' is module of type ' + get_module_type_string(module)),
+        print (' connected to gpio ' + str(module[2]))
+    else:
+        print (module[3] + ' is module of type ' + get_module_type_string(module)),
+        print (' available at address ' + module[4])
--- script.py
+++ script.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python2.7
 import os.path as path
+import json
 from WRH_Engine.RegisterDevice import RegisterDevice as register
 from WRH_Engine.Configuration import configuration as config
 
 CONFIGURATION_FILE = '.wrh.config'
 
 def _run_maintenance_work():
-    print 'Looks like your account has already been registered. Good!'
+    with open(CONFIGURATION_FILE, 'r') as f:
+        (system_info, modules) = config.parse_configuration_file(f)
+        for module in modules :
+            config.print_module_information(module)
     return
 
 def _is_configuration_file_sane():
@@ -21,14 +25,14 @@ def _is_configuration_file_present():
 def show_options():
     print 'Welcome to Wild Raspberry House management program!'
     if(_is_configuration_file_present() and _is_configuration_file_sane()):
-        print "All good"
         _run_maintenance_work()
     else:
         (is_success, response_content) = register.register_procedure()
         if(is_success):
             print 'Congratulations! Your device has successfully been registered!'
             with open(CONFIGURATION_FILE, 'w') as f:
-                f.write(response_content)
+                system_info = json.loads(response_content)
+                f.write(str(system_info['Id']) + ";" + str(system_info['Token']))
 
 if __name__ == "__main__":
     show_options()
