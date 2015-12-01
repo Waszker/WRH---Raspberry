e7de8c0611be8369f8ba29b990e1d7fd05991248
rzadp <rzadp@student.mini.pw.edu.pl>
2015-12-01 03:07:09 +0100

    poczatek MOVEMENT i WIFISOCKET

---
+++ WRH_Engine/Modules/MOVEMENT/MOVEMENT.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2.7
+
+import json
+import threading
+import time
+import socket
+# // import WebApiLibrary
+
+#na sztywno dane rasberaka uzytkownika przem321@wp.pl
+deviceid = '9'
+devicetoken = 'dea763a0-5c0c-4555-bcc6-9f0cc1dcf030'
+
+	
+def main():
+	print('main() start')
+	
+	print('main() end')
+
+if __name__ == "__main__":
+    main()
