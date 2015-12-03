3f01132107a9040fb963f2b9a0406f9dd417ed89
rzadp <rzadp@student.mini.pw.edu.pl>
2015-12-03 23:15:00 +0100

    commands

--- WRH_Engine/Modules/OVERLORD/wrh_lib.c
+++ WRH_Engine/Modules/OVERLORD/wrh_lib.c
@@ -8,10 +8,13 @@
 static enum module_type module_types[] = { DHT, CAMERA, MOTION, SOCKET };
 static int modules_number = sizeof(module_types) / sizeof(enum module_type);
 
+// https://trello.com/c/1oOLJIQW/60-module-type
 static command commands[] = {
     { { NULL }, { NULL } },
     { { NULL }, { "/usr/bin/python2.7", "-m", "WRH_Engine.Modules.CAMERA.camera", NULL } },
-    { { NULL }, { "/usr/bin/python2.7", "-m", "WRH_Engine.ScenarioManager.ScenarioManager", NULL } },
+	{ { NULL }, { "/usr/bin/python2.7", "-m", "WRH_Engine.Modules.MOVEMENT.MOVEMENT", NULL } },
+	{ { NULL }, { "/usr/bin/python2.7", "-m", "WRH_Engine.Modules.WIFISOCKET.WIFISOCKET", NULL } }, // gniazdko
+    { { NULL }, { "/usr/bin/python2.7", "-m", "WRH_Engine.ScenarioManager.ScenarioManager", NULL } }, 
     { { NULL }, { "./lcd2.py", NULL } },
     { { NULL }, { "/bin/stunnel", "./stunnel.conf", NULL } },
 };
