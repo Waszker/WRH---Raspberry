58feddfed1a3a67e9868b165a51bfe45b59f8206
root <waszka23@gmail.com>
2015-12-12 16:50:32 +0000

    Refactored camera module

--- WRH_Engine/Modules/CAMERA/camera.py
+++ WRH_Engine/Modules/CAMERA/camera.py
@@ -24,27 +24,28 @@ def _snapshot_thread(port, login, password):
 def _signal_handler(signal, frame):
     sys.exit(0)
 
-if __name__ == "__main__":
-    signal.signal(signal.SIGINT, _signal_handler)
-    conf_line = sys.argv[2]
-    camera = C.get_module_entry_data(conf_line)
-    device_name = camera.gpio
-    port = camera.address
-
+def _start_camera_thread(device_name, port):
     os.environ['LD_LIBRARY_PATH'] = '/usr/lib'
     command = ["/bin/mjpg_streamer",  "-i", "input_uvc.so -n -q 50 -f 1 -d " + str(device_name),
                "-o", "output_http.so -p " + port + " -c login:password"]
-    print command
+    print(command)
 
     # Preparing thread and subprocess
     thread = threading.Thread(target = _snapshot_thread, args = (port,'login','password'))
     p = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE, env = os.environ)
-
-    # Start thread as daemon
     thread.daemon = True
     thread.start()
 
+    return p
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, _signal_handler)
+    conf_line = sys.argv[2]
+
+    camera = C.get_module_entry_data(conf_line)
+    process = _start_camera_thread(camera.gpio, camera.address)
+
     # Await some response from subprocess
-    for line in p.stderr.readlines():
-        print line,
-    p.wait();
+    for line in process.stderr.readlines():
+        print(line),
+    process.wait();
