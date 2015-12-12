bf09377ca36a9a54cc7642118eb8c8fcca527888
rzadp <rzadp@student.mini.pw.edu.pl>
2015-12-12 22:28:55 +0000

    25 gpio

--- WRH_Engine/Modules/TEMPERATURE/temperature.py
+++ WRH_Engine/Modules/TEMPERATURE/temperature.py
@@ -2,5 +2,5 @@
 import sys
 import Adafruit_DHT
 
-humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 25)
 print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity)
