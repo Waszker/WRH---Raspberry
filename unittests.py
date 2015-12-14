c5760b84d15aca88268bbed8c32481f549b38b84
rzadp <rzadp@student.mini.pw.edu.pl>
2015-12-14 16:24:46 +0100

    unittests.py - testng method in scenariomanager as a simple example

--- unittests.py
+++ unittests.py
@@ -1,10 +1,14 @@
 import unittest
-import WRH_Engine.ScenarioManager.ScenarioManager
+import WRH_Engine.ScenarioManager.ScenarioManager as sm
 
 class TestScenarioManager(unittest.TestCase):
 
-  def test_stub(self):
-      self.assertEqual(1 == 1, True)
+	def test_stub(self):
+		self.assertEqual(1 == 1, True)
+
+	def test_convert_datetime_to_python_correct_date(self):
+		(success, datetime) = sm._convert_datetime_to_python("2015-12-14T16:23:30")
+		self.assertEqual(success, True)
 
 
 if __name__ == '__main__':
