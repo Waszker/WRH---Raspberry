4dcf4cdb330eca3b30bc02faa3427a1ba15a9e77
rzadp <rzadp@student.mini.pw.edu.pl>
2016-01-03 21:25:42 +0100

    Scenario Manager refactoring continues

--- WRH_Engine/ScenarioManager/ScenarioManager.py
+++ WRH_Engine/ScenarioManager/ScenarioManager.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python2.7
+from WRH_Engine.WebApiLibrary import Response
 from ..WebApiLibrary import WebApiClient as webapi
 from WRH_Engine.Configuration import configuration as config
 import WRH_Engine.Modules.CAMERA.camera as camera
@@ -17,42 +18,47 @@ from datetime import datetime, timedelta
 # Module responsible for managing Scenarios assigned to this Device
 # This Module is started by the Overlord
 # Scenario Manager periodically checks for updated Scenarios...
-# ...and communicated with other running Modules, to see if any of their most recent Measurements...
-# ...matches any of the Scenarios...
+# ...and communicates with other running Modules, to see if any of their most recent Measurements...
+# ...matches any of the Scenarios' Conditions...
 # ...if yes, then Scenario is executed, and Execution object uploaded
 
 
 # region OPTIONS
 
+
 CONFIGURATION_FILE = '.wrh.config'
 socket_port = 2000
+scenarios_changed_wait_time = 5  # (seconds) how often check with WebApi if Scenarios has changed
 
 # endregion ~OPTIONS
 
 
 # region GLOBAL VARIABLES
 
+
 device_id = ''
 device_token = ''
 scenarios = []
-measurements = dict() # // pairs, moduleId and Value
-doneScenarios = dict() # // pairs, scenarioId - number of times the scenario was executed
+measurements = dict()  # // moduleId - Value
+doneScenarios = dict()  # // scenarioId - number of times the scenario was executed
 lock = threading.Lock()
-event = threading.Event() # triggered when scenarios changed OR some measurement meet some scenarios' conditions
+event = threading.Event()  # triggered when scenarios changed OR some measurement meet some scenarios' conditions
 available_modules = []
+
 
 # endregion ~GLOBAL VARIABLES
 
 
 # region STARTUP AND CONFIGURATION METHODS
 
+
 def signal_handler(signal, frame):
     print 'Scenario Manager shutting down...'
     sys.exit(0)
 
 
-# read deviceId, deviceToken and Modules from configuration file
-def _read_available_modules():
+# read deviceId, deviceToken and modules from configuration file
+def _read_configuration_file():
     global available_modules
     global device_id
     global device_token
@@ -62,31 +68,18 @@ def _read_available_modules():
     device_token = system_info[1]
 
 
-# TODO: is it used? if so, is it working?
-# get streaming address, port, login and password encoded into one field - streamingaddress
-def _extract_info_from_streamingaddress(streaming_address):
-    # TODO zrobic to (Piotrek zrobi)
-    # we have encoded into camera module's streamingaddress four things:
-    address = "" # actual streaming address
-    port = ""
-    login = "login"
-    password = "password" # login and password are needed to make a snapshot
-    return (address, port, login, password)
-
-
 # endregion ~STARTUP AND CONFIGURATION METHODS
 
 
 # region COMMUNICATION WITH OTHER RUNNING MODULES
 
 
-# wait for incoming connections, and accept them. Accepted connection are then handled by _socket_communicate()
+# wait for incoming connections (from running Modules), and accept them.
+# accepted connection are then handled by _socket_communicate()
 def _socket_accept():
     global socket_port
-    print('accept_socket_messages() start')
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('localhost', socket_port))
-    print('_socket_accept bind: ' + str('localhost') + ' ' + str(socket_port))
     server_socket.listen(5)
     while 1:
         (client_socket, address) = server_socket.accept()
@@ -94,60 +87,57 @@ def _socket_accept():
         t.daemon = True
         t.start()
 
-    print('accept_socket_messages() end')
 
-
-# communicate with client (some Module in our case). Read measurement from it
-def _socket_communicate(clientsocket):
-    print('socket_communicate() start')
+# communicate with client (some Module in our case).
+# receive its most recent measurement
+# and update global measurements table
+def _socket_communicate(client_socket):
     global measurements
-    # read measurement, update global measurements array
-    module_id = clientsocket.recv(4096)
-    print('przyszedl measurement od modulu o id: ' + str(module_id))
-    clientsocket.send('ACK')
-    value = clientsocket.recv(4096)
-    print('przyszla wartosc rowna: ' + str(value))
+    module_id = client_socket.recv(4096)
+    client_socket.send('ACK')
+    value = client_socket.recv(4096)
+
     lock.acquire()
     measurements[str(module_id)] = value
-    scenarios_to_execute = _get_scenarios_to_execute()
+    scenarios_to_execute = _get_scenarios_to_execute()  # check what Scenarios (if any) are triggered
     lock.release()
-    if len(scenarios_to_execute) > 0:
+    if len(scenarios_to_execute) > 0:  # if there is at least one Scenario triggered, then set the event
         event.set()
-    print('socket_communicate() end')
+
 
 # endregion ~COMMUNICATION WITH OTHER RUNNING MODULES
 
 
 # region UPDATE SCENARIOS
 
-# periodically check with WebApi if scenarios has changed. If yes, then trigger event.
+
+# periodically check with WebApi if scenarios has changed. If yes, then set the event.
 def _scenarios_changed():
-    print('scenarios_changed() start')
     while True:
-        time.sleep(5) #TODO magic number
+        time.sleep(scenarios_changed_wait_time)
         (status_code, result_content) = webapi.scenarios_changed(device_id, device_token)
-        # check if scenarios changed, signal main() if yes (signal via Event)
-        # event.set()
-        # then exit, will be started again by main()
-        if status_code == 200 : #TODO use enum
-            print('SCENARIOS HAS CHANGED!')
+        if status_code == Response.STATUS_OK:
             event.set()
-            break
-    print('scenarios_changed() end')
+            break  # when event is set then break the loop and finish the method, it will be started again
+        else:
+            print 'SM: webapi.scenarios_changed returned status code ' \
+                  + str(status_code) \
+                  + '. (in method _scenarios_changed'
 
 
 # download Scenarios from WebApi
+# lock must be acquired when invoking this method
 def _get_scenarios():
-    # lock is acquired
     global scenarios
-    print('getting scenarios')
     (status_code, result_content) = webapi.get_scenarios(device_id, device_token)
-    if status_code != 200: # TODO magic number
-        print('_get_scenarios() status_code=' + str(status_code))
+    if status_code != Response.STATUS_OK:
+        print 'SM: webapi.get_scenarios returned status code ' \
+                  + str(status_code) \
+                  + '. (in method _get_scenarios'
         scenarios = []
         return
     result_object = json.loads(result_content)
-    scenarios = result_object
+    scenarios = result_object  # lock is acquired
 
 
 # endregion ~UPDATE SCENARIOS
@@ -191,15 +181,16 @@ def _get_scenarios_to_execute():
 
     for scen in active_scenarios:
         if not str(scen["ConditionModuleId"]) in measurements:
-            continue # there is no Measurement from Module with this Id (yet)
+            continue  # there is no Measurement from Module with this Id (yet)
         value = measurements[str(scen["ConditionModuleId"])]
-        print('>> sprawdzam scenariusze, ostatnia wartosc dla modulu o id: ' + str(scen["ConditionModuleId"]) + ' wynosi: ' + str(value))
+        print('>> sprawdzam scenariusze, ostatnia wartosc dla modulu o id: ' + str(
+                scen["ConditionModuleId"]) + ' wynosi: ' + str(value))
 
         if not str(scen["Id"]) in doneScenarios:
             doneScenarios[str(scen["Id"])] = 0
         done = doneScenarios[str(scen["Id"])]
-        if done>0 and int(scen["Recurring"])==0:
-            continue # // scenario was already executed, and is not recurring
+        if done > 0 and int(scen["Recurring"]) == 0:
+            continue  # // scenario was already executed, and is not recurring
 
         conditionMet = False
         temp = ''
@@ -207,38 +198,41 @@ def _get_scenarios_to_execute():
         if scen["Condition"] < 5:
             v = value.split(';')
             if len(v) != 2:
-                continue # not properly encoded value.
+                continue  # not properly encoded value.
             temp = v[0]
             wilg = v[1]
 
-        if scen["Condition"] == 1: # Temperatura ponizej..
+        if scen["Condition"] == 1:  # Temperatura ponizej..
             if temp < scen["ValueInt"]:
                 conditionMet = True
-        if scen["Condition"] == 2: # Temperatura powyzej..
+        if scen["Condition"] == 2:  # Temperatura powyzej..
             if temp > scen["ValueInt"]:
                 conditionMet = True
-        if scen["Condition"] == 3: # Wilgotnosc ponizej..
+        if scen["Condition"] == 3:  # Wilgotnosc ponizej..
             if wilg < scen["ValueInt"]:
                 conditionMet = True
-        if scen["Condition"] == 4: # Wilgotnosc powyzej..
+        if scen["Condition"] == 4:  # Wilgotnosc powyzej..
             if wilg > scen["ValueInt"]:
                 conditionMet = True
-        if scen["Condition"] == 5: # Wykryto ruch
+        if scen["Condition"] == 5:  # Wykryto ruch
             print('Condition scenariusza - czy wykryto ruch?')
             if int(value) > 0:
-                print('value=' + str(value)+ '> 0, czyli tak')
+                print('value=' + str(value) + '> 0, czyli tak')
                 conditionMet = True
         if conditionMet:
             result.append(scen)
 
     return result
 
+
 # endregion ~DETERMINE SCENARIOS TO BE EXECUTED
 
 
 # region SCENARIO EXECUTION
 
-# try to execute all scenarios taken from _get_scenarios_to_execute()
+# get a list of Scenarios which are supposed to be executed
+# and try to execute them
+# also, upload Execution object
 def _try_execute_scenarios():
     global measurements
     global doneScenarios
@@ -286,13 +280,14 @@ def _execute_scenario(actionmoduleid, action):
         return True
     if action == '4':
         # TODO take snapshot
-        print('taking snapshot, port=' + str(module.address) +' login:password')
+        print('taking snapshot, port=' + str(module.address) + ' login:password')
         content = camera.get_camera_snapshot(str(module.address), "login", "password")
         print('snapshot taken, content = ')
         print(str(content))
         print('TODO: wyslac snapshot jako measurement')
         return True
     return False
+
 
 # endregion ~SCENARIO EXECUTION
 
@@ -328,9 +323,8 @@ def _main_event_waiting():
 
 
 def main():
-
     print('Scenario Manager started')
-    _read_available_modules()
+    _read_configuration_file()
 
     _get_scenarios()
     print str(len(scenarios))
