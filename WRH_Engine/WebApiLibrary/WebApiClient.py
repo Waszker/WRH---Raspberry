1d687c2ec4a17b544fdc2c95a430d0b6c507e1a0
rzadp <rzadp@student.mini.pw.edu.pl>
2016-01-11 00:25:56 +0100

    Added WebApiClient comments

--- WRH_Engine/WebApiLibrary/WebApiClient.py
+++ WRH_Engine/WebApiLibrary/WebApiClient.py
@@ -3,6 +3,9 @@ import requests
 import json
 from enum import Enum
 
+# WebApiClient - Module used for communication with Web Api
+# Available Web Api methods are documented here: https://wildraspberrywebapi.azurewebsites.net/swagger/ui/index#/
+
 
 class Response(Enum):
     STATUS_OK = 200
@@ -23,7 +26,7 @@ add_measurement_url = base_address + 'api/wrh/addmeasurement'
 scenarios_changed_url = base_address + 'api/wrh/scenarioschanged'
 add_execution_url = base_address + 'api/wrh/addexecution'
 
-
+# Generic method for making HTTP POST requests
 def do_post_request(url, content):
     try:
         response = requests.post(url, data = json.dumps(content), headers = headers)
@@ -33,42 +36,43 @@ def do_post_request(url, content):
 
     return result
 
-
+# Register Device in the WRH system
 def register_device(username, password, device_name, device_color):
     content = {'Username': username, 'Password': password, 'Name': device_name, 'Color': device_color}
     return do_post_request(register_device_url, content)
 
-
+# Add a new Module
 def add_module(device_id, device_token, module_name, module_type):
     content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Name': module_name, 'Type': module_type}
     return do_post_request(add_module_url, content)
 
-
+# Edit an existing Module
 def edit_module(device_id, device_token, module_id, module_name):
     content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Id': module_id, 'Gpio': 0, 'Name': module_name}
     return do_post_request(edit_module_url, content)
 
-
+# Remove an existing Module
 def remove_module(device_id, device_token, module_id):
     content = {'DeviceId': device_id, 'Devicetoken': device_token, 'Id': module_id}
     return do_post_request(remove_module_url, content)
 
-
+# Get Scenarios for this Device
 def get_scenarios(device_id, device_token):
     content = {'Id': device_id, 'Token': device_token}
     return do_post_request(get_scenarios_url, content)
 
-
+# Add a new Measurement
+# streamingaddress is optional, used for specifying Camera's address
 def add_measurement(device_id, device_token, module_id, timestamp, value, streamingaddress):
     content = {'DeviceId': device_id, 'Devicetoken': device_token, 'ModuleId': module_id, 'Timestamp': timestamp, 'Value': value, 'StreamingAddress': streamingaddress}
     return do_post_request(add_measurement_url, content)
 
-
+# Check if list of Scenarios has changed
 def scenarios_changed(device_id, device_token):
     content = {'Id': device_id, 'Token': device_token}
     return do_post_request(scenarios_changed_url, content)
 
-
+# Add a new Scenario Execution
 def add_execution(device_id, device_token, value_condition, value_action, timestamp, scenario_id, condition, action):
     content = {'DeviceId': device_id, 'Devicetoken': device_token, 'ValueCondition': value_condition, 'ValueAction': value_action, 'Timestamp': timestamp, 'ScenarioId': scenario_id, 'Condition': condition, 'Action': action }
     return do_post_request(add_execution_url, content)
