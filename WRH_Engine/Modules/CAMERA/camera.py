0f7538726a5817568c58da63ac064d786a0af843
Piotr Waszkiewicz <waszka23@gmail.com>
2016-07-01 22:31:20 +0200

    Additional changes in Tornado 2

--- WRH_Engine/Modules/CAMERA/camera.py
+++ WRH_Engine/Modules/CAMERA/camera.py
@@ -138,7 +138,7 @@ class CameraModule(base_module.Module):
         :param website_host_address: ip address of server
         :return:
         """
-        return "< img src = \"http://" + website_host_address + ":" + self.address + "/?action=stream\" / >"
+        return "<div><img src = \"http://" + website_host_address + ":" + self.address + "/?action=stream\" /></div>"
 
     def _start_stunnel(self):
         filename = "/tmp/stunnel" + str(self.id) + ".conf"
