e71aaae34830148552315414ca5a868d89c31256
Piotr Waszkiewicz <waszka23@gmail.com>
2016-07-04 23:55:14 +0200

    Fixes in Overlord

--- WRH_Engine/Modules/OVERLORD/overlord.py
+++ WRH_Engine/Modules/OVERLORD/overlord.py
@@ -7,18 +7,23 @@ from sets import Set
 import WRH_Engine.Configuration.configuration as c
 import WRH_Engine.Utils.dynamic_loader as d
 
+__CONFIG_FILE__ = '.wrh_config'
+
 
 class Overlord:
     """
     Overlord program takes care of all subprocesses registered in WRH.
     """
-    processes = Set()
+    processes = []
+    commands = []
 
     def __init__(self, configuration_filename):
         with open(configuration_filename, 'r') as f:
             modules_classes = d.scan_and_return_module_classes("WRH_Engine/Modules/")
             (_, modules) = c.parse_configuration_file(f, modules_classes)
             self.modules = modules
+            self.configuration_lines = []
+            for line in f: self.configuration_lines.append(line)
         signal.signal(signal.SIGINT, _siginit_handler)
         signal.signal(signal.SIGCHLD, _sigchld_handler)
 
@@ -28,30 +33,33 @@ class Overlord:
         their lifetime.
         :return:
         """
-        tornado_command = ["/usr/bin/python2.7", "-m", "WRH_Engine.Modules.TORNADO.server"]
-        Overlord.processes.add((subprocess.Popen(tornado_command), tornado_command))
+        tornado_command = ["/usr/bin/python2.7", "-m", "WRH_Engine.Modules.TORNADO.server", __CONFIG_FILE__]
+        Overlord.processes.append(subprocess.Popen(tornado_command))
+        Overlord.commands.append(tornado_command)
 
+        index = 1
         for m in self.modules:
-            process = subprocess.Popen(m.get_starting_command())
-            Overlord.processes.add((process, m.get_starting_command()))
+            command = m.get_starting_command.append(self.configuration_lines[0], self.configuration_lines[index])
+            process = subprocess.Popen(command)
+            Overlord.processes.append(process)
+            Overlord.commands.append(command)
+            index += 1
 
 
-def _siginit_handler(signal, frame):
+def _siginit_handler(_, _):
     print 'OVERLORD: SIGINT signal caught'
     for p in Overlord.processes:
         os.kill(p, signal.SIGINT)
     sys.exit(0)
 
 
-def _sigchld_handler(signal, frame):
-    zombies = Set()
-    for (process, command) in Overlord.processes:
+def _sigchld_handler(_, _):
+    for i in enumerate(0, len(Overlord.processes)):
+        process, command = Overlord.processes[i], Overlord.commands[i]
         process_info = psutil.Process(process.pid)
         if process_info.status() == psutil.STATUS_ZOMBIE:
             process.wait()
-            zombies.add((process, command))
-            zombies.add((subprocess.Popen(command), command))
-    Overlord.processes = Overlord.processes.symmetric_difference_update(zombies)
+            Overlord.processes[i] = subprocess.Popen(command)
 
 
 if __name__ == "__main__":
@@ -59,7 +67,7 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, _siginit_handler)
     signal.signal(signal.SIGCHLD, _sigchld_handler)
 
-    overlord = Overlord('.wrh_config')
+    overlord = Overlord(__CONFIG_FILE__)
     overlord.start_work()
     while True:
         signal.pause()
