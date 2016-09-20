55e5ce40512f90bb600d75c782e32f5561e9dcc1
Piotr Waszkiewicz <waszka23@gmail.com>
2016-09-20 02:15:59 +0200

    Huge changes to Tornado 7

--- WRH_Engine/Modules/ESP8266_WIFI_SOCKET/esp8266_socket.py
+++ WRH_Engine/Modules/ESP8266_WIFI_SOCKET/esp8266_socket.py
@@ -171,15 +171,15 @@ class ESP8266SocketModule(base_module.Module):
         text_input_name = "esp_timeout_" + self.id
         return '<div class="card-panel"> \
                 <h5>' + self.name + '</h5>\
-                <div id="esp8266SocketDiv' + self.id + '" class="socketDiv"> \
+                <div id="esp8266SocketDiv' + self.id + '" class="socketDiv" style="width: 200px; height: 200px"> \
                 <img src="static/images/loading_spinner.gif" style="width: 50px;" /> \
                 </div> \
                 <br /> \
                 <table style="margin: 0px auto; max-width: 95%; width: auto"><tr> \
-                <td><div style="margin: 3%; width: 100%"><p class="range-field"><input id=\"' + text_input_name + '\" type="range" style="width: 90%" value="-1" min="-1" max="100"/></p></div></td> \
-                <td><button class="waves-effect waves-light btn grey darken-3" type="button" onclick="setState' + self.id + '(\'ON\', \'' + text_input_name + '\')">ON</button></td> \
-                <td><button class="waves-effect waves-light btn grey darken-3" type="button" onclick="setState' + self.id + '(\'OFF\', \'' + text_input_name + '\')">OFF</button></td> \
-                <td><button class="waves-effect waves-light btn grey darken-3" type="button" onclick="getState' + self.id + '()">REFRESH</button></td> \
+                <td rowspan="3"><div style="margin: 3%; width: 100%"><p class="input-field"><input id=\"' + text_input_name + '\" type="number" style="width: 90%" value="-1"/></p></div></td> \
+                </tr><tr><td><button class="waves-effect waves-light btn grey darken-3" type="button" onclick="setState' + self.id + '(\'ON\', \'' + text_input_name + '\')">ON</button></td> \
+                </tr><tr><td><button class="waves-effect waves-light btn grey darken-3" type="button" onclick="setState' + self.id + '(\'OFF\', \'' + text_input_name + '\')">OFF</button></td> \
+                </tr><tr><td><button class="waves-effect waves-light btn grey darken-3" type="button" onclick="getState' + self.id + '()">REFRESH</button></td> \
                 </tr></table> \
                 <script> \
                 function update_state_message' + self.id + '(text) \n { \
