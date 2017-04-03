0f22434f1f242403c7aacb098b0b25e83861a7a8
Piotr Waszkiewicz <waszka23@gmail.com>
2017-04-03 16:20:18 +0200

    Changed DHT22 module ending routine

--- modules/dht22/dht22.py
+++ modules/dht22/dht22.py
@@ -184,6 +184,7 @@ class DHT22Module(base_module.Module):
     def _await_connection(self):
         self.socket.listen(10)
         while self.should_end is False:
+            log("Awaiting on " + str(socket), Color.BLUE)
             connection, address = self.socket.accept()
             if self.last_temperature is not None and self.last_humidity is not None:
                 connection.send('{0:0.1f};{1:0.1f}'.format(self.last_humidity, self.last_temperature))
@@ -194,7 +195,7 @@ class DHT22Module(base_module.Module):
     def _sigint_handler(self, *_):
         self.should_end = True
         if self.socket is not None:
-            log("Clossing socket", Color.BLUE)
+            log("Clossing socket " + str(self.socket), Color.BLUE)
             self.socket.close()
 
 
