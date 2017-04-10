1ef0d17467e81a13e4147bbefe5b47c7b30027d1
waszker <waszka23@gmail.com>
2017-04-10 15:29:32 +0200

    Fixed issues in sockets library

--- utils/sockets.py
+++ utils/sockets.py
@@ -1,28 +1,28 @@
 import time
 import socket as s
+from io import log, Color
 
 """
 Set of functions to make socket handling a lot easier.
 """
 
-def wait_bind_socket(host, port, sleep, retries=-1, predicate=lambda: True,  error_message=None):
+def wait_bind_socket(socket, host, port, sleep, retries=-1, predicate=lambda: True,  error_message=None):
     """
     Tries to bind socket for specified host and port.
     In case of failure the procedure is retried until predicate value is False or provided
     number of retries is achieved. After each failure the procedure sleeps for a sleep number
     of seconds.
     """
-    socket = s.socket(s.AF_INET, s.SOCK_STREAM)
     while predicate() and retries != 0:
         try:
-            socket = s.bind((str(host), int(port)))
+            socket.bind((str(host), int(port)))
             break
         except s.error as message:
             if error_message is not None:
-                print str(error_message) + str(message)
+                log(str(error_message) + str(message), Color.FAIL)
             time.sleep(sleep)
             retries = max(-1, retries-1)
-    return socket if (predicate() and retries != 0) else None
+    return predicate() and retries != 0
 
 def await_connection(socket, callback, predicate=lambda: True):
     """
